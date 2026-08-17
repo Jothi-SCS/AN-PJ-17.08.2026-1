b12bd2c63f490346c181fc9e6edc5cc4f0d4ef7c
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-08-17 12:36:40 +0530

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,14 @@
+-- CollegeDB - Department Table Assignment
+-- Student Name:
+-- Register Number:
+
+-- Write your SQL code below.
+
+-- Requirements:
+-- 1. Create database CollegeDB
+-- 2. Use CollegeDB
+-- 3. Create Department table
+-- 4. DepartmentID INT(5) PRIMARY KEY
+-- 5. DepartmentName VARCHAR(20)
+-- 6. HOD VARCHAR(20)
+
