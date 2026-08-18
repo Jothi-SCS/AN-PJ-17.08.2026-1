cd32701019d22f6c50447b1ac6cd316418627947
sumanbaskar <suman2862008@gmail.com>
2026-08-18 14:31:15 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE universitysDB;
 
+USE universitysDB;
+
+CREATE TABLE Department (
+    DepartmentID NUMERIC(5) PRIMARY KEY,
+    DepartmentName VARCHAR(20) NOT NULL,
+    HOD VARCHAR(20) NOT NULL
+);
+
+DESCRIBE Department;
